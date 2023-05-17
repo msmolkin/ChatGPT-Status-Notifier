ad58eb7debc60342504b9265a980587bf06a59da
Michael <michael@smolkin.org>
2023-05-17 16:11:06 -0700

    Add checkbox to enable sound directly to popup

--- popup.js
+++ popup.js
@@ -2,3 +2,13 @@
 document.querySelector('#optionsButton').addEventListener('click', () => {
   browser.runtime.openOptionsPage();
 });
+
+// When the popup page is loaded, populate the checkbox with the current setting
+browser.storage.local.get('playSound').then((result) => {
+  document.querySelector('#soundCheckbox').checked = result.playSound;
+});
+
+// Save the setting whenever the checkbox is toggled
+document.querySelector('#soundCheckbox').addEventListener('change', (event) => {
+  browser.storage.local.set({ playSound: event.target.checked });
+});
